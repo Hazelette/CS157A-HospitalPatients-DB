411d3e2f22276452d89b206f385e3c41195fcbe0
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-04-30 15:05:52 -0700

    chore: cleanup unnecessary files after refactor

--- backend/hospital/src/main/java/com/example/hospital/repository/AdmissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hospital.repository;
-
-import com.example.hospital.model.Admission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdmissionRepository extends JpaRepository<Admission, Integer> {
-}
