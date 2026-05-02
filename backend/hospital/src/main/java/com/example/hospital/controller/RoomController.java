d0f73c7c67dd897e6da58b985593cadba6a9be7c
Hazelette <62589987+Hazelette@users.noreply.github.com>
2026-05-02 16:17:23 -0700

    ensure that tables are viewable on frontend

--- backend/hospital/src/main/java/com/example/hospital/controller/RoomController.java
+++ backend/hospital/src/main/java/com/example/hospital/controller/RoomController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
 public class RoomController {
 
     private final RoomDAO roomDAO;
